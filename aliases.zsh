4acffdd18c2ab137d2f40b079704d5ab5ca9d5c4
Dimitri Depardieu <dimitridepardieu@gmail.com>
2024-03-01 21:38:51 +0100

    Add aliases

---
+++ aliases.zsh
@@ -0,0 +1,10 @@
+# Shortcuts
+alias dev="cd ~/Developer"
+alias dl="cd ~/Downloads"
+alias py="python3"
+
+# Get external IP in JSON format
+alias myip="curl https://ipinfo.io/json; echo" # /ip for oneline plain text
+
+# Run a network speedtest
+alias speedtest="networkQuality"
