0da4609b09ddd287fe3d8f710c43aac372b5c7e3
Dimitri Depardieu <dimitridepardieu@gmail.com>
2025-03-20 21:29:31 +0100

    neovim: Reminder to add telescope keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -949,6 +949,8 @@ require('lazy').setup({
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  -- TODO: Add some good keymaps!
+  -- Example: :TodoTelescope or :TodoQuickFix
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
